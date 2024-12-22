54ea6c7fe2072a42e0d3b27dfabc42ba05be3567
uc02 <upendrachauhan0206@gmail.com>
2024-12-22 23:12:05 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-
+function Square({ value }){
+  return <button className="square">{value}</button>;
+}
 
 
 function App() {
@@ -6,7 +8,21 @@ function App() {
 
   return (
     <>
-      
+      <div className="board-row">
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
+      </div>
+      <div className="board-row">
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
+      </div>
+      <div className="board-row">
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
+      </div>
     </>
   )
 }
